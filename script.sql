4ea6ed5236cd726710881540d930c376f7e5457f
Manuel Franceschi <95531044+manuelfranceschi@users.noreply.github.com>
2024-10-24 09:23:12 +0200

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,4 +1,4 @@
---DROP TABLE IF EXISTS campus,proyectos,promociones,verticales,alumnos,claustro,resultado_proyectos,promocion_asociada, CASCADE;
+DROP TABLE IF EXISTS campus,proyectos,promociones,verticales,alumnos,claustro,resultado_proyectos,promocion_asociada,alum_prom CASCADE;
 
 -- Crear tabla campus .
 CREATE TABLE campus(
@@ -74,47 +74,3 @@ CREATE TABLE promocion_asociada(
   FOREIGN KEY (id_promocion) REFERENCES promociones(id_promocion),
   FOREIGN KEY (id_campus) REFERENCES campus(id_campus)
 );
-
-
--- -- Insertar datos en tabla authors
--- INSERT INTO authors(name,surname,email,image)
--- VALUES
--- ('Alejandru','Regex','alejandru@thebridgeschool.es','https://randomuser.me/api/portraits/thumb/men/75.jpg'),
--- ('Birja','Rivera','birja@thebridgeschool.es','https://randomuser.me/api/portraits/thumb/men/60.jpg'),
--- ('Alvaru','Riveru','alvaru@thebridgeschool.es','https://randomuser.me/api/portraits/thumb/men/45.jpg'),
--- ('Muchelle','Wuallus','muchelle@thebridgeschool.es','https://randomuser.me/api/portraits/thumb/women/72.jpg'),
--- ('Albertu','Henriques','albertu@thebridgeschool.es','https://randomuser.me/api/portraits/thumb/men/33.jpg'),
--- ('Guillermu','Develaweyer','guillermu@thebridgeschool.es','https://randomuser.me/api/portraits/thumb/men/34.jpg'),
--- ('Jabier','Hespinoza','jabier@thebridgeschool.es','https://randomuser.me/api/portraits/thumb/men/35.jpg');
-
-
-
--- -- Insertar datos en tabla entries
--- INSERT INTO entries(title,content,id_author,category)
--- VALUES 
--- ('Noticia: SOL en Madrid','Contenido noticia 1',(SELECT id_author FROM authors WHERE email='alejandru@thebridgeschool.es'),'Tiempo'),
--- ('Noticia: Un panda suelto por la ciudad','El panda se comió todas las frutas de una tienda',(SELECT id_author FROM authors WHERE email='birja@thebridgeschool.es'),'Sucesos'),
--- ('El rayo gana la champions','Victoria por goleada en la final de la champions',(SELECT id_author FROM authors WHERE email='albertu@thebridgeschool.es'),'Deportes'),
--- ('Amanece Madrid lleno de arena','La calima satura Madrid de arena. Pérdidas millonarias',(SELECT id_author FROM authors WHERE email='birja@thebridgeschool.es'),'Sucesos'),
--- ('Descubren el motor de agua','Fin de la gasolina. A partir de ahora usaremos agua en nuestros coches',(SELECT id_author FROM authors WHERE email='alvaru@thebridgeschool.es'),'Ciencia');
-
--- -- Buscar entries por email usuario
--- SELECT * FROM entries WHERE id_author=(SELECT id_author FROM authors WHERE email='alejandru@thebridgeschool.es');
-
-
--- -- Buscar datos por email de usuario y cruzar datos
--- SELECT e.title,e.content,e.date,e.category,a.name,a.surname,a.image
--- FROM entries AS e
--- INNER JOIN authors AS a
--- ON e.id_author=a.id_author
--- WHERE a.email='alejandru@thebridgeschool.es'
--- ORDER BY e.title;
-
-
--- -- Buscar datos por email de 2 usuarios y cruzar datos
--- SELECT entries.title,entries.content,entries.date,entries.category,authors.name,authors.surname,authors.image
--- FROM entries
--- INNER JOIN authors
--- ON entries.id_author=authors.id_author
--- WHERE authors.email='alejandru@thebridgeschool.es' OR authors.email='alvaru@thebridgeschool.es' OR authors.email='albertu@thebridgeschool.es'
--- ORDER BY entries.title;
